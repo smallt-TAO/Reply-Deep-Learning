c87bc48662ecc73b6e0bec27ab204f0de48b6e58
smallt-TAO <llt5662720@163.com>
2016-12-20 19:14:16 +0800

    Update

--- Lasagne/mnist_mlp.py
+++ Lasagne/mnist_mlp.py
@@ -136,7 +136,7 @@ def build_cnn(input_var=None):
 
 def main(model='mlp', num_epochs=500):
     print("Loading data...")
-    X_train, y_train, X_val, y_val, X_text, y_test = load_data_set()
+    X_train, y_train, X_val, y_val, X_test, y_test = load_data_set()
 
     input_var = T.tensor4('inputs')
     target_var = T.ivector('targets')
@@ -152,3 +152,61 @@ def main(model='mlp', num_epochs=500):
     else:
         print("Unrecognized model type %r." % model)
         return
+    prediction = lasagne.layers.get_output(network)
+    loss = lasagne.objectives.categorical_crossentropy(prediction, target_var).mean()
+
+    params = lasagne.layers.get_all_params(network, trainable=True)
+    updates = lasagne.updates.nesterov_momentum(loss, params, learning_rate=0.01, momentum=0.9)
+
+    test_prediction = lasagne.layers.get_output(network, deterministic=True)
+    test_loss = lasagne.objectives.categorical_crossentropy(test_prediction, target_var).mean()
+
+    test_acc = T.mean(T.eq(T.argmax(test_prediction, axis=1), target_var),
+                      dtype=theano.config.floatX)
+
+    train_fn = theano.function([input_var, target_var], loss, updates=updates)
+    val_fn = theano.function([input_var, target_var], [test_loss, test_acc])
+
+    print("Starting training...")
+    for epoch in range(num_epochs):
+        train_err = 0
+        train_batches = 0
+        start_time = time.time()
+        for batch in iterate_mini_batches(X_train, y_train, 500, shuffle=True):
+            inputs, targets = batch
+            train_err += train_fn(inputs, targets)
+            train_batches += 1
+
+        val_err = 0
+        val_acc = 0
+        val_batches = 0
+        for batch in iterate_mini_batches(X_val, y_val, 500, shuffle=False):
+            inputs, targets = batch
+            err, acc = val_fn(inputs, targets)
+            val_err += err
+            val_acc += acc
+            val_batches += 1
+
+        print("Epoch {} of {} took {:.3f}s".format(epoch + 1, num_epochs, time.time() - start_time))
+        print("  training err:\t\t{:.6f}".format(train_err))
+        print("  training loss:\t\t{:.6f}".format(train_err / train_batches))
+        print("  validation loss:\t\t{:.6f}".format(val_err / val_batches))
+        print("  validation accuracy:\t\t{:.2f} %".format(val_acc / val_batches * 100))
+
+    # After training, we compute and print the test error:
+    test_err = 0
+    test_acc = 0
+    test_batches = 0
+    for batch in iterate_mini_batches(X_test, y_test, 500, shuffle=False):
+        inputs, targets = batch
+        err, acc = val_fn(inputs, targets)
+        test_err += err
+        test_acc += acc
+        test_batches += 1
+    print("Final results:")
+    print("  test loss:\t\t\t{:.6f}".format(test_err / test_batches))
+    print("  test accuracy:\t\t{:.2f} %".format(test_acc / test_batches * 100))
+
+
+if __name__ == '__main__':
+    main()
