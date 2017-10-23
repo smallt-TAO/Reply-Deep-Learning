ebec772909b1d6b4dcb49a8b0c89ae65945021d4
Lintao Lv <llt5662720@163.com>
2017-10-23 19:42:04 +0800

    Update data.py

--- VIN/data.py
+++ VIN/data.py
@@ -8,7 +8,7 @@ def process_gridworld_data(input, imsize):
     # state_data: concatenated one-hot vectors for each state variable
     # state_xy_data: state variable (x,y position)
     # label_data: one-hot vector for action (state difference)
-    im_size=[imsize, imsize]
+    im_size = [imsize, imsize]
     matlab_data = sio.loadmat(input)
     im_data = matlab_data["batch_im_data"]
     im_data = (im_data - 1)/255  # obstacles = 1, free zone = 0
@@ -16,18 +16,21 @@ def process_gridworld_data(input, imsize):
     state1_data = matlab_data["state_x_data"]
     state2_data = matlab_data["state_y_data"]
     label_data = matlab_data["batch_label_data"]
+    
+    # Save data in other data type
     ydata = label_data.astype('int8')
     Xim_data = im_data.astype('float32')
     Xim_data = Xim_data.reshape(-1, 1, im_size[0], im_size[1])
     Xval_data = value_data.astype('float32')
     Xval_data = Xval_data.reshape(-1, 1, im_size[0], im_size[1])
     Xdata = np.append(Xim_data, Xval_data, axis=1)
+    
     # Need to transpose because Theano is NCHW, while TensorFlow is NHWC
     Xdata = np.transpose(Xdata,  (0, 2, 3, 1))
     S1data = state1_data.astype('int8')
     S2data = state2_data.astype('int8')
 
-    all_training_samples = int(6/7.0*Xdata.shape[0])
+    all_training_samples = int(6 / 7.0 * Xdata.shape[0])
     training_samples = all_training_samples
     Xtrain = Xdata[0:training_samples]
     S1train = S1data[0:training_samples]
@@ -39,11 +42,13 @@ def process_gridworld_data(input, imsize):
     S2test = S2data[all_training_samples:]
     ytest = ydata[all_training_samples:]
     ytest = ytest.flatten()
-
+    
+    # re permutation the samples.
     sortinds = np.random.permutation(training_samples)
     Xtrain = Xtrain[sortinds]
     S1train = S1train[sortinds]
     S2train = S2train[sortinds]
     ytrain = ytrain[sortinds]
     ytrain = ytrain.flatten()
+    
     return Xtrain, S1train, S2train, ytrain, Xtest, S1test, S2test, ytest
