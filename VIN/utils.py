bfcad31c2061e5db49f15f2660dbce3badbd4967
Lintao Lv <llt5662720@163.com>
2017-10-23 19:43:49 +0800

    Update utils.py

--- VIN/utils.py
+++ VIN/utils.py
@@ -4,7 +4,7 @@ import tensorflow as tf
 # helper methods to print nice table (taken from CGT code)
 def fmt_item(x, l):
     if isinstance(x, np.ndarray):
-        assert x.ndim==0
+        assert x.ndim == 0
         x = x.item()
     if isinstance(x, float): rep = "%g"%x
     else: rep = str(x)
@@ -18,5 +18,4 @@ def flipkernel(kern):
     return kern[(slice(None, None, -1),) * 2 + (slice(None), slice(None))]
 
 def conv2d_flipkernel(x, k, name=None):
-    return tf.nn.conv2d(x, flipkernel(k), name=name,
-                        strides=(1, 1, 1, 1), padding='SAME')
+    return tf.nn.conv2d(x, flipkernel(k), name=name, strides=(1, 1, 1, 1), padding='SAME')
