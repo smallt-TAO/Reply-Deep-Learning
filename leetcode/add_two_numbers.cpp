681299f3a7ec5cc1384b3a1e819ba02aa53b9a3f
smalltao <smalltao@ali-0F84ED84-E2F7-5FA9-B2F5-FB113DAD1E49.local>
2017-12-01 09:57:04 +0800

    Remmber Hulu failed

---
+++ leetcode/add_two_numbers.cpp
@@ -0,0 +1,28 @@
+# leetcode 2
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
+        ListNode* res = new ListNode(0);
+        ListNode* ss = res;
+        int temp = 0;
+        while (l1 || l2 || temp) {
+            int l1Sum = l1 ? l1 -> val : 0;
+            int l2Sum = l2 ? l2 -> val : 0;
+            int sum = l1Sum + l2Sum + temp;
+            ss -> next = new ListNode(sum % 10);
+            temp = sum / 10;
+            ss = ss -> next;
+            l1 = l1 ? l1 -> next : l1;
+            l2 = l2 ? l2 -> next : l2;
+        }
+        return res -> next;
+    }
+};
