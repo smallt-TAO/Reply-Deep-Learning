ac24c0dabc26d49b0826e6b5cd9e358511a104f0
smalltao <smalltao@ali-0F84ED84-E2F7-5FA9-B2F5-FB113DAD1E49.local>
2017-12-27 19:32:04 +0800

    prefix and suffix search

--- leetcode/745_Prefix_and_Suffix_Search.cpp
+++ leetcode/745_Prefix_and_Suffix_Search.cpp
@@ -15,4 +15,67 @@ struct Trie {
 		}
 	}
 
+	void add(const string &word, size_t begin, int index) {
+		words.push_back(index);
+		if (begin < word.length()) {
+			if (!children[word[begin] - 'a']) {
+				children[word[begin] - 'a'] = new Trie();
+			}
+			children[word[begin] - 'a'] -> add(word, begin + 1, index);
+		}
+	}
+
+	vector<int> find(const string &prefix, size_t begin) {
+		if (beign == prefix.length()) {
+			return words;
+		}
+		else {
+			if (!children[prefix[begin] - 'a']) {
+				return {};
+			}
+			else {
+				return children[prefix[begin] - 'a'] -> find(prefix, begin + 1);
+			}
+		}
+	}
+};
+
+
+class WordFilter {
+public:
+        WordFilter(vector<string> words) {
+        forwardTrie = new Trie();
+        backwardTrie = new Trie();
+        for (int i = 0; i < words.size(); i++) {
+            string word = words[i];
+            forwardTrie->add(word, 0, i);
+            reverse(word.begin(), word.end());
+            backwardTrie->add(word, 0, i);
+        }
+    }
+
+    int f(string prefix, string suffix) {
+        auto forwardMatch = forwardTrie->find(prefix, 0);
+        reverse(suffix.begin(), suffix.end());
+        auto backwardMatch = backwardTrie->find(suffix, 0);
+        // search from the back
+        auto fIter = forwardMatch.rbegin(), bIter = backwardMatch.rbegin();
+        while (fIter != forwardMatch.rend() && bIter != backwardMatch.rend()) {
+            if (*fIter == *bIter) {
+                return *fIter;
+            } else if (*fIter > *bIter) {
+                fIter ++;
+            } else {
+                bIter ++;
+            }
+        }
+        return -1;
+    }
+
+private:
+    Trie *forwardTrie, *backwardTrie;
+};
+
+
+
 	
