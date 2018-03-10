f8a6adc9f7dc04534fcbabd0b6b4ea89dab3268c
吕琳涛 <smalltao@lvlintaodeMacBook-Pro.local>
2018-03-10 12:44:09 +0800

    add the detail about regular expression matching

--- leetcode/010_regular_expression_matching.cpp
+++ leetcode/010_regular_expression_matching.cpp
@@ -1,17 +1,31 @@
 #include <iostream>
+#include <string>
 
 using namespace std;
 
 class Solution {
     public:
     bool checkMatch(string s, string p) {
+        if (p.empty())  return s.empty();
 
+        if (p[1] == '*') {
+            return checkMatch(s, p.substr(2)) ||
+                   (!s.empty() && (s[0] == p[0] || '.' == p[0]) && checkMatch(s.substr(1), p));
+        }
+        else {
+            return !s.empty() && (s[0] == p[0] || '.' == p[0]) && checkMatch(s.substr(1), p.substr(1));
+        }
 
     }
 
 };
 
 int main() {
+    string s("aa");
+    string p("a*");
+    Solution sol;
+    cout << sol.checkMatch(s, p) << endl;
+
     return 0;
 }
 
