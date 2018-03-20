a56a937386a050109e0f8e8929234ab926fcd883
吕琳涛 <smalltao@lvlintaodeMacBook-Pro.local>
2018-03-20 23:48:04 +0800

    Fuck the binary search

---
+++ leetcode/032_longest_valid_parentheses.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int longestValidParentheses(string s) {
+        if (s.size() == 0) return 0;
+        
+        stack<int> st;
+        int res = 0;
+        st.push(-1);
+        for (int i = 0; i < s.size(); i++) {
+            if (s[i] == '(') {
+                st.push(i);
+            }
+            else {
+                st.pop();
+                if (st.empty()) {
+                    st.push(i);
+                }
+                else {
+                    res = max(res, i - st.top());
+                }
+            }
+        }
+        
+        return res;
+    }
+};
