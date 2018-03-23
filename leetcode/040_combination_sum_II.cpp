d44f5c1b76fdff31d4db09b694a9a772aa0f7c4e
吕琳涛 <smalltao@lvlintaodeMacBook-Pro.local>
2018-03-23 21:47:56 +0800

    Add combination sum II

---
+++ leetcode/040_combination_sum_II.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    vector<vector<int>> combinationSum2(vector<int>& candidates, int target) {
+        vector<vector<int>> res;
+        vector<int> te;
+        sort(candidates.begin(), candidates.end());
+        if (candidates.size() == 0) return res;
+        
+        help(res, te, candidates, 0, target, 0);
+        
+        return res;
+    }
+    
+    void help(vector<vector<int> >& res, vector<int> temp, vector<int> candidates, int sum, int target, int start) {
+        if (sum > target) return;
+        if (sum == target) {
+            res.push_back(temp);
+            return;
+        }
+        
+        for (int i = start; i < candidates.size(); i++) {
+            if (i > start && candidates[i] == candidates[i - 1]) continue;
+            temp.push_back(candidates[i]);
+            help(res, temp, candidates, sum + candidates[i], target, i + 1);
+            temp.pop_back();
+        }
+    }
+};
